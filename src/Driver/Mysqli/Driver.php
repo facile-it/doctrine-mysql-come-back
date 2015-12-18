527a134df723bfb1bc0a68ba7b8ef1ef5b0432e8
Zack Brenton <zack@jnickel.com>
2015-12-18 12:05:50 -0400

    moved ServerGoneAwayExceptionsAwareInterface and added a Mysqli driver that implements that interface

---
+++ src/Driver/Mysqli/Driver.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) 2015 Nickel Media Inc.
+ * Created by IntelliJ IDEA.
+ * User: zackbrenton
+ * Date: 2015-12-18
+ * Time: 11:50 AM
+ */
+
+namespace Driver\Mysqli;
+
+
+use Facile\DoctrineMySQLComeBack\Doctrine\DBAL\Driver\PDOMySql\ServerGoneAwayExceptionsAwareInterface;
+
+class Driver extends \Doctrine\DBAL\Driver\Mysqli\Driver implements ServerGoneAwayExceptionsAwareInterface
+{
+    /**
+     * @var array
+     */
+    protected $goneAwayExceptions = array(
+        'MySQL server has gone away'
+    );
+
+    /**
+     * @param \Exception $exception
+     * @return bool
+     */
+    public function isGoneAwayException(\Exception $exception)
+    {
+        $message = $exception->getMessage();
+
+        foreach ($this->goneAwayExceptions as $goneAwayException) {
+            if (strpos($message, $goneAwayException) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
