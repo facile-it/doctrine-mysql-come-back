bfd22c6ab57ae08b76fb492605063bc87a2546e3
Markus Geiger <mg@evolution515.net>
2016-04-04 11:41:32 +0200

    Better detection of MySQL server has gone away
    "PDO::query(): MySQL server has gone away" could also be thrown.

--- src/Driver/PDOMySql/Driver.php
+++ src/Driver/PDOMySql/Driver.php
@@ -11,25 +11,14 @@ use Facile\DoctrineMySQLComeBack\Doctrine\DBAL\Driver\ServerGoneAwayExceptionsAw
 class Driver extends \Doctrine\DBAL\Driver\PDOMySql\Driver implements ServerGoneAwayExceptionsAwareInterface
 {
     /**
-     * @var array
-     */
-    protected $goneAwayExceptions = array(
-        'SQLSTATE[HY000]: General error: 2006 MySQL server has gone away',
-        'PDOStatement::execute(): MySQL server has gone away'
-    );
-
-    /**
      * @param \Exception $exception
      * @return bool
      */
     public function isGoneAwayException(\Exception $exception)
     {
         $message = $exception->getMessage();
-
-        foreach ($this->goneAwayExceptions as $goneAwayException) {
-            if (strpos($message, $goneAwayException) !== false) {
-                return true;
-            }
+        if (strpos($message, 'MySQL server has gone away') !== false) {
+            return true;
         }
 
         return false;
