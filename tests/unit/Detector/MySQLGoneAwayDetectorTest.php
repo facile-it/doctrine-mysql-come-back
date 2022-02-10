242a76235fdb567d9f3059a0470e636e27a5a1a4
Alessandro Lai <alessandro.lai85@gmail.com>
2022-02-10 16:37:55 +0100

    Refactor unit tests into dedicated class for GoneAwayDetector

--- tests/unit/Detector/MySQLGoneAwayDetectorTest.php
+++ tests/unit/Detector/MySQLGoneAwayDetectorTest.php
@@ -9,4 +9,37 @@ class MySQLGoneAwayDetectorTest extends TestCase
 {
     use ProphecyTrait;
 
+    private const RETRYABLE_ERROR = 'Lost connection to MySQL server during query is an error not retryable on UPDATE queries';
+
+    private const RETRYABLE_ERROR_OUTSIDE_UPDATE = 'Lost connection to MySQL server during query is an error not retryable on UPDATE queries';
+
+    /**
+     * @dataProvider isUpdateQueryDataProvider
+     */
+    public function testIsUpdateQuery(string $query, bool $isUpdate): void
+    {
+        $error = new \Exception(self::RETRYABLE_ERROR_OUTSIDE_UPDATE);
+
+        $this->assertSame(! $isUpdate, (new MySQLGoneAwayDetector())->isGoneAwayException($error, $query));
+        $this->assertTrue((new MySQLGoneAwayDetector())->isGoneAwayException($error, 'SELECT 1'));
+    }
+
+    public function isUpdateQueryDataProvider(): array
+    {
+        return [
+            ['UPDATE ', true],
+            ['DELETE ', true],
+            ['DELETE ', true],
+            ['SELECT ', false],
+            ['select ', false],
+            ["\n\tSELECT\n", false],
+            ['(select ', false],
+            [' (select ', false],
+            [' 
+            (select ', false],
+            [' UPDATE WHERE (SELECT ', true],
+            [' UPDATE WHERE 
+            (select ', true],
+        ];
+    }
 }
