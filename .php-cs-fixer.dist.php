cbf825fc65c0682f2d2c3d2c476da08345152fe2
Alessandro Lai <alessandro.lai85@gmail.com>
2022-03-25 10:38:17 +0100

    Disable phpdoc_to_comment CS rule to avoid issues with Psalm annotations

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,10 @@
  * These rules will be added to default rules or will override them if the same key already exists.
  */
  
-$additionalRules = [];
+$additionalRules = [
+    // see https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
+    'phpdoc_to_comment' => false,
+];
 $rulesProvider = new Facile\CodingStandards\Rules\CompositeRulesProvider([
     new Facile\CodingStandards\Rules\DefaultRulesProvider(),
     new Facile\CodingStandards\Rules\ArrayRulesProvider($additionalRules),
