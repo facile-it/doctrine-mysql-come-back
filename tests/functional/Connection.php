c341fa40b7e85515af26b8151fe5e57fd81418ba
Alessandro Lai <alessandro.lai85@gmail.com>
2022-03-25 10:30:55 +0100

    Raise Psalm level to 2

--- tests/functional/Connection.php
+++ tests/functional/Connection.php
@@ -6,6 +6,7 @@ namespace Facile\DoctrineMySQLComeBack\Doctrine\DBAL\FunctionalTest;
 
 class Connection extends \Facile\DoctrineMySQLComeBack\Doctrine\DBAL\Connection
 {
+    /** @var int */
     public $connectCount = 0;
 
     public function connect(): bool
